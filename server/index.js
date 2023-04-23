b1c90749779792cf800e2ba0bb1a14c3f5578038
Mijael <mijael691@gmail.com>
2023-04-23 01:00:24 -0500

    Backend service implemented

--- server/index.js
+++ server/index.js
@@ -3,15 +3,37 @@ import morgan from "morgan";
 import { Server as SocketServer } from "socket.io";
 import http from "http";
 import cors from "cors";
+// import { dirname} from "path";
+// import { fileURLToPath } from "url"
 
 import { PORT } from "./config.js";
 
 const app = express();
+// const __dirname = dirname(fileURLToPath(import.meta.url))
+// console.log(__dirname)
 const server = http.createServer(app);
-const io = new SocketServer(server);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "http://127.0.0.1:5173",
+  },
+});
 
 app.use(cors());
 app.use(morgan("dev"));
 
-app.listen(PORT);
+io.on("connection", socket => {
+  console.log(socket.id);
+
+  socket.on("message", (message) => {
+    console.log(message)
+    socket.broadcast.emit("message", {
+        body: message,
+        from: socket.id
+    })
+  });
+});
+
+// app.use(express.static(__dirname, '../build'))
+
+server.listen(PORT);
 console.log("Server started on port", PORT);
